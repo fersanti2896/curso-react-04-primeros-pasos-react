ab78e46caee0cd4b27d988722f1bf22a624604a8
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-07 01:12:46 -0600

    Comunicacion entre componentes y uso de props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,10 +1,13 @@
 /* Functionals Components */
 import React from 'react'
 
-const PrimeraApp = () => {
+const PrimeraApp = ( {saludo = 'Hola mundo!'}) => {
+    // const saludo2 = { name: 'Fernado', age: 24};
+
     return (
         <>
-            <h1> Hola mundo </h1>
+            <h1> { saludo } </h1>
+            {/* <pre> {JSON.stringify(saludo, null, 3)} </pre> */}
             <p>Este es mi primera pagina</p>
         </>
     );
