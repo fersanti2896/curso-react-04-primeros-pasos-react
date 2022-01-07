f0562782f7563d76546451653d696d58fa8787e3
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-07 00:44:48 -0600

    Retornando elementos en el componente PrimeraApp()

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,7 +2,12 @@
 import React from 'react'
 
 const PrimeraApp = () => {
-    return <h1> Hola mundo </h1>;
+    return (
+        <>
+            <h1> Hola mundo </h1>
+            <p>Este es mi primera pagina</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
