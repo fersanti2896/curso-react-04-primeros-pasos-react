d0cd5af2980c627dc3e50324353f3fbab38b3378
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-07 02:48:43 -0600

    Uso del useState - Hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,18 @@
 /* Functional Component */
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ( {value} ) => {
+    const [counter, setCounter] = useState(0);
+
     const handleAdd = (e) => {
-        console.log(e);
+        setCounter(counter + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
 
             <button onClick={ handleAdd }>+1</button>
         </>
